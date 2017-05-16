89a63c8233115674dcf056c95814cafbc6178ec5
Mark Piper <mark.piper@colorado.edu>
2017-05-16 12:14:55 -0600

    Implement BMI get_grid_origin method
    See also 88256bd.

--- cruAKtemp/bmi_cruAKtemp.py
+++ cruAKtemp/bmi_cruAKtemp.py
@@ -244,6 +244,10 @@ class BmiCruAKtempMethod(object):
     def get_grid_spacing(self, grid_id):
         return np.array([10000.0, 10000.0], dtype='float32')
 
+    # Todo: Revise once we can work with georeferenced data in the CMF.
+    def get_grid_origin(self, grid_id):
+        return np.array([0.0, 0.0], dtype='float32')
+
     def get_grid_rank(self, var_id):
         return len(self.get_grid_shape(var_id))
 
